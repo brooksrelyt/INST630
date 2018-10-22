48d6fa24818d1b26c1a6dee6c85b56d6991ed69d
Tyler Brooks <brooksrelyt@gmail.com>
2018-10-22 13:06:50 -0400

    new content

---
+++ HW Assignment 2/brooks_a2_q3.js
@@ -0,0 +1,37 @@
+var planets = [
+  {planet: 'Mercury', position: '1', orbit_time: '0.24', nat_satellites: '0'},
+  {planet: 'Venus', position: '2', orbit_time: '0.62', nat_satellites: '0'},
+  {planet: 'Earth', position: '3', orbit_time: '1', nat_satellites: '1'},
+  {planet: 'Mars', position: '4', orbit_time: '1.88', nat_satellites: '2'},
+  {planet: 'Jupiter', position: '5', orbit_time: '11.86', nat_satellites: '67'},
+  {planet: 'Saturn', position: '6', orbit_time: '29.46', nat_satellites: '62'},
+  {planet: 'Uranus', position: '7', orbit_time: '84.32', nat_satellites: '27'},
+  {planet: 'Neptune', position: '8', orbit_time: '164.79', nat_satellites: '14'}
+];
+
+function myNewFunction(sel) {
+  i = sel.options[sel.selectedIndex].text;
+  document.write(planets[i].planet + ' is planet #' + planets[i].position + 
+      ' from the Sun. Time to complete its orbit is ' + planets[i].orbit_time + ' earth year(s). It has ' + planets[i].nat_satellites + ' natural satellite(s).<br>' );
+}
+
+// var listPlanets = function (userInput) {
+//   var sel = document.getElementById("numPicker");
+//   var text = sel.options[sel.selectedIndex].text;
+
+//   for (var i = 0; i < planets.length; i++) {
+//     if (planets[i].position == userInput) {
+//       document.write(planets[i].planet + ' is planet #' + planets[i].position + 
+//       ' from the Sun. Time to complete its orbit is ' + planets[i].orbit_time + ' earth year(s). It has ' + planets[i].nat_satellites + ' natural satellite(s).<br>' ); 
+//     }
+//   }
+// }
+
+// num = window.prompt("Please enter a number between 1 and 8");
+
+// if (1 <= num && num <= 8) {
+//   listPlanets(num);
+// } else {
+//   alert("The value you entered is not within range. Please reload the page and enter a value thatis within 1 and 8, inclusive."); 
+//   window.location.reload();
+// }

e79a42dcf772ce280b990c7b65c9e6e1d6217bc3
Tyler Brooks <brooksrelyt@gmail.com>
2018-10-26 16:15:41 -0400

    added tooltip

--- final-proj-dev/js/map.js
+++ final-proj-dev/js/map.js
@@ -14,20 +14,47 @@ map._initPathRoot()
 var svg = d3.select("#map").select("svg"),
 g = svg.append("g");
 
-d3.json("../data/circles.json", function(collection) {
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width",300)
+  .attr("height",300);
+  
+var tooltip = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style('opacity', 0)
+  .style('position', 'absolute')
+  .style('padding', '0 10px');
+
+
+d3.json("../data/yelp-fusion.json", function(collection) {
   // Add a LatLng object to each item in the dataset 
-  collection.objects.forEach(function(d) {
-    d.LatLng = new L.LatLng(d.circle.coordinates[0],
-                d.circle.coordinates[1])
+  collection.businesses.forEach(function(d) {
+    d.LatLng = new L.LatLng(d.coordinates.latitude,
+                d.coordinates.longitude)
   })
   
   var feature = g.selectAll("circle")
-    .data(collection.objects)
+    .data(collection.businesses)
     .enter().append("circle")
     .style("stroke", "blue")  
     .style("opacity", .6) 
     .style("fill", "blue")
-    .attr("r", 10);  
+    .attr("r", 10)
+    .on("mouseover", function(d) {
+      var html  = d.name + "<br><hr>" + "Star Rating: " + d.rating + "<br>" + "Total Review: " + d.review_count;
+      tooltip.html(html)
+        .style('background', '#fff')
+        .style("left", (d3.event.pageX + 25) + "px")
+        .style("top", (d3.event.pageY - 30) + "px")
+      .transition()
+        .duration(200) // ms
+        .style("opacity", .9) // started as 0!
+    })
+    .on("mouseout",function(d) {
+       tooltip.transition()
+        .duration(300) // ms
+        .style("opacity", 0); // don't care about position!
+    })
   
   map.on("viewreset", update);
   update();
@@ -42,3 +69,39 @@ d3.json("../data/circles.json", function(collection) {
     )
   }
 })  
+
+
+//////////////////////////////////// -----------------------------------------------------------------
+// var svg = d3.select("body")
+//   .append("svg")
+//   .attr("width",300)
+//   .attr("height",300);
+  
+// var tooltip = d3.select("body").append("div")
+//   .attr("class", "tooltip")
+//   .style('opacity', 0)
+//   .style('position', 'absolute')
+//   .style('padding', '0 10px');
+  
+// svg.selectAll("circle") // empty selection
+//   .data(pointsData)     // data to bind to selection
+//   .enter()              // the enter selection holds new elements in the selection
+//   .append("circle")     // append a circle for each new element
+//   // Manage the style of each circle based on its datum:
+//   .attr("cx",function(d) { return d.location[0]; })
+//   .attr("cy",function(d) { return d.location[1]; })
+//   .attr("r",10)
+//   .on("mouseover", function(d) {
+//     tooltip.transition()
+//       .style('opacity', .9)
+//       .style('background', 'steelblue')
+//       .text("Business: " + d.businessName + ". performance " + d.performance)
+//       .style('left', (d3.event.pageX - 35) + 'px')
+//       .style('top', (d3.event.pageY - 30) + 'px')
+//       .duration(100);
+//   })
+//   .on("mouseout",function(d) {
+//      tooltip.transition()
+//        .style("opacity", "0")
+//        .duration(50);
+//   })

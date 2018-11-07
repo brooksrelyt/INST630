68fa8dc3d86d3a72e200b119c33e14ce0b966082
Tyler Brooks <brooksrelyt@gmail.com>
2018-11-07 15:53:03 -0500

    updated and finished mt

--- midterm/brooks_mt_04.js
+++ midterm/brooks_mt_04.js
@@ -7,7 +7,7 @@ document.getElementById('AG').onchange = function() {
  		document.getElementById("genPara").innerHTML = "Would you like a Picture book or a Chapter book?";
  		document.getElementById("gen2").innerHTML = "Picture";
  		document.getElementById("gen3").innerHTML = "Chapter";
- 		// recom.innerHTML = " ";
+ 		recom.innerHTML = " ";
  		document.getElementById('GN').selectedIndex = 0;
  	} else if (children == 2) {
  		document.getElementById("genPara").innerHTML = "Would you like a Fiction book or a Nonfiction book?";
@@ -15,7 +15,14 @@ document.getElementById('AG').onchange = function() {
  		document.getElementById("gen3").innerHTML = "Non-fiction";
  		recom.innerHTML = " ";
  		document.getElementById('GN').selectedIndex = 0;
- 		alert('its 2');
+ 		// alert('its 2');
+
+
+ 		var opt = document.getElementById('GN').options[1];
+	    opt.value =  'hack1';
+
+	    var opt = document.getElementById('GN').options[2];
+	    opt.value =  'hack2';
  	}   
 };
 
@@ -28,5 +35,9 @@ function recomText(sel) {
 	}
 	else if ( sel.options[sel.selectedIndex].value == "3" ) {
 		recom.innerHTML = "I recommend 'The Secret Garden' by Frances Hodgson Burnett";
+	} else if ( sel.options[sel.selectedIndex].value == "hack1" ) {
+		recom.innerHTML = "I recommend 'Frankenstein' by Mary Shelley";
+	} else if ( sel.options[sel.selectedIndex].value == "hack2" ) {
+		recom.innerHTML = "I recommend 'A Brief History of Time' by S. Hawking";
 	}
 }
